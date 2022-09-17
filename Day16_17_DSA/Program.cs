07f35de7a99d4090ee114c3bd01815b589f99b89
Abhishek bhavekar <bhavekarabhi@gmail.com>
2022-09-17 23:23:29 +0530

    [Abhi]Added:Program

---
+++ Day16_17_DSA/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Day16_17_DSA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an option ");
+            Console.WriteLine("1.Permutations of string");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter string for permutation: ");
+                    string arr = Console.ReadLine();
+                    Console.WriteLine("Permutation of string is: ");
+                    
+                    Permutations.PerformPermutation(arr, 0, arr.Length - 1);
+
+                    
+                    Console.WriteLine("Enter first string:");
+                    string arr1 = Console.ReadLine();
+                    Console.WriteLine("Enter second string:");
+                    string arr2 = Console.ReadLine();
+                    CheckArrayString.CheckString(arr1, arr2);
+                    Console.WriteLine("string is" + CheckArrayString.CheckString(arr1, arr2));
+                    break;
+            }
+        }
+    }
+}
